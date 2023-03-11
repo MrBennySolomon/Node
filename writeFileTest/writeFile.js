dcd45e2e28cdab8e620af31a5c636dbcb80a1717
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-11 22:32:13 +0200

    writeFile

--- writeFileTest/writeFile.js
+++ writeFileTest/writeFile.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My name is Benny 2.');
+//fs.writeFileSync('notes.txt', 'My name is Benny 2.');
+fs.appendFileSync('notes.txt', ' APPENDING ...');
