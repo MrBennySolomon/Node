41016e304c2d84a99de760267897a90fd36b9748
MrBennySolomon <MrBennySolomon@gmail.com>
2023-03-15 13:25:34 +0200

    pureNode

--- pureNode/server.js
+++ pureNode/server.js
@@ -19,9 +19,9 @@ const server = http.createServer((req, res) => {
       );
     }
 
-    if (req.url === "/about") {
+    if (req.url === "/raw-html") {
       fs.readFile(
-        path.join(__dirname, "public", "about.html"),
+        path.join(__dirname, "public", "raw.html"),
         (err, content) => {
           if (err) throw err;
           res.writeHead(200, { "Content-Type": "text/html" });
